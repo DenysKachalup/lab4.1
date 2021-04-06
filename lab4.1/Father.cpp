47d5d6ac4fcc27ef60a9c21a24cf1869502accdb
Denys-Maksym Kachalup <Denys-Maksym.Kachalup.ITIS.2020@edu.lpnu.ua>
2021-04-06 11:29:08 +0300

    lab

---
+++ lab4.1/Father.cpp
@@ -0,0 +1,15 @@
+#include "Father.h"
+Father::Father(string n)
+	:name(n)
+{}
+
+Father::Father() 
+	:name(" ") 
+{}
+
+void Father::PrintName()
+{
+	cout << "Name : " << getN();
+}
+
+
---
+++ lab4.1/Father.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Father
+{
+private:
+	string name;
+public:
+	Father();
+	Father(string n);
+	void setN(string n) { name = n; }
+	string getN() const { return name; }
+	
+	virtual void PrintName();
+};
+
